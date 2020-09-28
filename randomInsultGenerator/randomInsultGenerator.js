22cdb9ffa30caa89e4579762d08adb2c3e666e11
yue <yue.liu@authright.com>
2020-09-27 22:48:27 -0400

    random-insult-generator

---
+++ randomInsultGenerator/randomInsultGenerator.js
@@ -0,0 +1,28 @@
+/*
+random insult generator
+
+The app creates three different arrays of body parts,
+adjectives and random words 
+and the mini app will turn them into a string that creates a random insult.
+*/
+
+// 1. create an array with four different body parts
+const randomBodyParts = ["head", "arms", "legs", "feet"];
+
+// 2. create an array with four adjectives 
+const randomAdjectives = ["long", "short", "large", "small"];
+
+// 3. create an array with five random words
+const randomWords = ["one", "two", "three", "four", "five"];
+
+// 4. use the Math.floor() method and the Math.random() method to choose a random item from your randomBodyParts array and assign it to the variable randomBodyPart
+const randomBodyPart = randomBodyParts[Math.floor(Math.random() * 3)];
+
+// 5. choose a random item from your randomAdjectives array and assign it to the variable randomAdjective
+const randomAdjective = randomAdjectives[Math.floor(Math.random() * 3)];
+
+// 6. choose a random item from your randomWord array and assign it to the variable randomWords
+const randomWord = randomWords[Math.floor(Math.random() * 5)];
+
+// 7. write a console.log statement that displays your randomly generated insult
+console.log(`Your ${randomBodyPart} is like a ${randomAdjective} ${randomWord}`);
