632851ce90bd413578cd81fe49d4ee42e1a5e7f4
yue <yue.liu@authright.com>
2020-10-07 14:17:16 -0400

    keycode-js

---
+++ getKeycode/main.js
@@ -0,0 +1,17 @@
+const logKey = (e) => {
+  const log = document.getElementById('log');
+  log.textContent += e.code;
+}
+
+const getKeycode = (e) => {
+  // let keycode;
+  // if (window.event) {
+  //   keycode += window.event.keycode;
+  // } else if (e) {
+  //   keycode = e.which;
+  // }
+  alert("keycode: " + e.which);
+}
+
+document.onkeyup = logKey;
+document.onkeydown = getKeycode;
